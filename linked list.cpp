b3381cc57a7ab3a482f878773bb78ef8beb7a45f
advityasingh <56801642+advityasingh@users.noreply.github.com>
2019-10-23 10:04:20 +0530

    Add files via upload

---
+++ linked list.cpp
@@ -0,0 +1,22 @@
+  
+#include<stdio.h>
+#include <stdlib.h>
+struct node
+{
+	int info;
+	struct node *next;
+};
+int main()
+{
+	struct node *temp1,*temp2;
+	temp1=(struct node*)malloc(sizeof(struct node));
+	temp2=(struct node*)malloc(sizeof(struct node));
+	
+	temp1->info=6;
+	temp1->next=temp2;
+	printf("Data = %d Present Address = %u Next Address = %u",temp1->info,temp1,temp1->next);
+	
+	temp2->info=8;
+	temp2->next=NULL;
+	printf("\nData = %d Present Address = %u Next Address = %u",temp2->info,temp2,temp2->next);
+}
